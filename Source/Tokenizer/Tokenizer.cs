aeae5f759608f646f4c2371373ecd128db32270b
TakoPuck <arthurqueste@gmail.com>
2026-04-19 14:52:46 +0200

    fix: tokenizer null argument

--- Source/Tokenizer/Tokenizer.cs
+++ Source/Tokenizer/Tokenizer.cs
@@ -14,8 +14,12 @@ public static class Tokenizer
                 return new TextToken(segment.Value);
 
             case SegmentType.OpenTag:
-                string[] parts = segment.Value.Split('=');
-                return new OpenTagToken(parts[0], parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries));
+                {
+                    string[] parts = segment.Value.Split('=');
+                    string name = parts.Length > 0 ? parts[0] : string.Empty;
+                    string[] args = parts.Length > 1 ? parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries) : [];
+                    return new OpenTagToken(name, args);
+                }
 
             case SegmentType.CloseTag:
                 return new CloseTagToken(segment.Value);
@@ -27,7 +31,9 @@ public static class Tokenizer
 
     public static IToken[] Tokenize(List<Segment> segments)
     {
-        IToken[] tokens = new IToken[segments?.Count ?? 0];
+        if (segments == null) return [];
+
+        IToken[] tokens = new IToken[segments.Count];
 
         for (int i = 0; i < segments.Count; i++)
         {
