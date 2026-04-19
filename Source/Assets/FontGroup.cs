41d67ef1e8d14b541cbd4cd471c3870ec2876f27
TakoPuck <arthurqueste@gmail.com>
2026-04-19 13:37:14 +0200

    feat: FontGroup object

---
+++ Source/Assets/FontGroup.cs
@@ -0,0 +1,43 @@
+﻿using MonoGame.Extended.BitmapFonts;
+
+namespace RichTextExtended.Source.Assets;
+
+public class FontGroup
+{
+    public BitmapFont Normal { get; }
+
+    public BitmapFont Bold { get; }
+
+    public BitmapFont Italic { get; }
+
+    public BitmapFont BoldAndItalic { get; }
+
+
+    public FontGroup(BitmapFont normal, BitmapFont bold = null, BitmapFont italic = null, BitmapFont boldAndItalic = null)
+    {
+        Normal = normal;
+        Bold = bold ?? normal;
+        Italic = italic ?? normal;
+        BoldAndItalic = boldAndItalic ?? normal;
+    }
+
+    public BitmapFont GetFont(bool bold, bool italic)
+    {
+        if (bold && italic)
+        {
+            return BoldAndItalic;
+        }
+
+        if (bold)
+        {
+            return Bold;
+        }
+
+        if (italic)
+        {
+            return Italic;
+        }
+
+        return Normal;
+    }
+}
