4638514dbda5762ab9a36359fc70a8e5507abad5
TakoPuck <arthurqueste@gmail.com>
2026-04-19 13:36:27 +0200

    feat: open tag token args getter methods

--- Source/Tokenizer/OpenTagToken.cs
+++ Source/Tokenizer/OpenTagToken.cs
@@ -13,6 +13,30 @@ public class OpenTagToken : IToken
         Args = args;
     }
 
+    private bool IsIndexOutOfBounds(int index) => index < 0 || index >= Args.Length;
+
+    public string GetArg(int index, string defaultValue = "")
+    {
+        if (IsIndexOutOfBounds(index))
+        {
+            return defaultValue;
+        }
+
+        return Args[index];
+    }
+
+    public bool TryGetArg(int index, out string value)
+    {
+        if (IsIndexOutOfBounds(index))
+        {
+            value = string.Empty;
+            return false;
+        }
+
+        value = Args[index];
+        return true;
+    }
+
     public override string ToString()
     {
         return $"Name: '{Name}', Args: '{string.Join("' '", Args)}'";
